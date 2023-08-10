003821d56836d22c8799118d3bcae23794e17996
Niklas <37415436+Fini04@users.noreply.github.com>
2023-08-10 12:29:12 +0200

    build

--- src/lib/date.ts
+++
@@ -1,9 +0,0 @@
-export function getCurrentDate() {
-  const date = new Date();
-  let currentDay = String(date.getDate()).padStart(2, "0");
-  let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
-  let currentYear = date.getFullYear();
-
-  // return date as YYYY-MM-DD
-  return `${currentYear}-${currentMonth}-${currentDay}`;
-}
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  define: { __API_KEY__: process.env.VITE_API_KEY },
-  plugins: [svelte()],
-});
