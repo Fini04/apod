0b5ad89d166af69714c636d5b4f3afa8e3d96d5a
Niklas <37415436+Fini04@users.noreply.github.com>
2023-08-12 15:16:51 +0200

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   define: { __API_KEY__: process.env.VITE_API_KEY },
   plugins: [svelte()],
-  base: "/apod/",
+  base: "/",
 });
